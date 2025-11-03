9d2c9044de95858d76158a607da48cb6217c1d6d
dipti <dipti.skills@gmail.com>
2025-11-03 14:08:39 -0500

    JpaGeneratorAgent

--- JpaGenerator/src/main/java/com/example/genai/devops/controller/JPAGeneratorController.java
+++ JpaGenerator/src/main/java/com/example/genai/devops/controller/JPAGeneratorController.java
@@ -2,63 +2,37 @@ package com.example.genai.devops.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.http.HttpRequest;
 import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/v1")
-public class SpPromptController {
+public class JPAGeneratorController {
 
     @Value("${spring.ai.azure.openai.chat.options.deployment-name}")
     private String deploymentName;
 
     private final ChatClient chatClient;
 
-    private final RestTemplate restTemplate;
-
-    public SpPromptController(ChatClient chatClient, RestTemplate restTemplate) {
+    public JPAGeneratorController(ChatClient chatClient) {
         this.chatClient = chatClient;
-        this.restTemplate = restTemplate;
     }
 
 
     @PostMapping
-    public String getPromptFromSp(@RequestParam String storedProd){
+    public String getJPAFromPrompt(@RequestParam String agentPrompt){
 
-        var respPrompt = chatClient
+        return chatClient
                 .prompt(loadPromptFile("/system_prompt.txt"))
-                .user(storedProd)
+                .user(agentPrompt)
                 .call()
                 .content();
-
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type","application/x-www-form-urlencoded");
-        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        map.add("agentPrompt",respPrompt);
-        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(map, headers);
-
-        return restTemplate.exchange(
-                "http://localhost:8080/api/v1",
-                HttpMethod.POST,
-                request,
-                String.class).getBody();
-
-
     }
 
     private String loadPromptFile(String fileName){
